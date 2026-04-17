b2e9c3c944fe19949968f89162f688b0fe496c56
Kiryang Kwon <KiryangKwon@gmail.com>
2026-04-17 03:01:07 -0500

    created java teller thread

--- BankSimulation.java
+++ BankSimulation.java
@@ -1,3 +1,15 @@
+import java.util.concurrent.Semaphore;
+import java.util.Random;
+
 public class BankSimulation {
-    
-}
+
+    static final int NUM_TELLERS = 3;
+    static final int NUM_CUSTOMERS = 50;
+
+    //Teller Thread
+    public static class Teller extends Thread {
+        int id;
+
+        Teller(int id) {
+            this.id = id;
+        }
