f7967d5c2df4a6e034292e0a1b2487472a6dab80
Kiryang Kwon <KiryangKwon@gmail.com>
2026-04-17 03:05:27 -0500

    added the skelton

--- BankSimulation.java
+++ BankSimulation.java
@@ -31,3 +31,29 @@ public class BankSimulation {
             System.out.println("Customer " + id + " [Customer " + id + "]: is entering the bank");
         }
     }
+
+    // main
+    public static void main(String[] args) throws InterruptedException {
+
+        Teller[] tellers = new Teller[NUM_TELLERS];
+        Customer[] customers = new Customer[NUM_CUSTOMERS];
+
+        // Start tellers first
+        for (int i = 0; i < NUM_TELLERS; i++) {
+            tellers[i] = new Teller(i);
+            tellers[i].start();
+        }
+
+        // Start customers
+        for (int i = 0; i < NUM_CUSTOMERS; i++) {
+            customers[i] = new Customer(i);
+            customers[i].start();
+        }
+
+        // Wait for everyone to finish
+        for (int i = 0; i < NUM_TELLERS; i++)    tellers[i].join();
+        for (int i = 0; i < NUM_CUSTOMERS; i++)  customers[i].join();
+
+        System.out.println("Bank is now closed.");
+    }
+}
